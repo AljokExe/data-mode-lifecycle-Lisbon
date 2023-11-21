6353440410dfc9561f8d47c8b99f89b57d81fc28
Alvaro Graca <alvaro.ps.graca@gmail.com>
2023-11-21 18:36:22 +0000

    end before class

--- taxifare/interface/main.py
+++ taxifare/interface/main.py
@@ -159,8 +159,10 @@ def train(
     save_model(model=model)
 
     # The latest model should be moved to staging
+    # $CHA_BEGIN
     if MODEL_TARGET == 'mlflow':
         mlflow_transition_model(current_stage="None", new_stage="Staging")
+    # $CHA_END
 
     print("✅ train() done \n")
 
